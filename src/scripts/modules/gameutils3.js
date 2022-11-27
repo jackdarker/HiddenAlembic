83ab77af49f3e4f5672307217030132a5e8788d8
Jack Darker <JackDarker@freenet.de>
2022-11-27 16:31:58 +0100

    ???

--- src/scripts/modules/gameutils3.js
+++ src/scripts/modules/gameutils3.js
@@ -3,8 +3,7 @@ window.gm.initGameFlags = function(forceReset,NGP=null) {
     let s= window.story.state,map,data;
     function dataPrototype(){return({visitedTiles:[],mapReveal:[],tmp:{},version:0});}
     if (forceReset) {  
-      s.Settings=s.DngCV=s.DngDF=s.DngAM=s.DngSY=s.DngMN=s.DngAT=null; 
-      s.DngFM=s.DngSC=s.DngLB=s.DngHC=s.DngPC=null;
+      s.Settings=s.DngSY=null; 
       s.Lab=s.Known=null;
     }
     let Settings = {
@@ -13,18 +12,16 @@ window.gm.initGameFlags = function(forceReset,NGP=null) {
       showDungeonMap:true
     };
     let DngSY = {
-        remainingNights: 0,
-        dngLevel: 1, //tracks the mainquest you have finished
-        dngOW: false, //if this flag is set while in dng, player is here for some freeplay (no quest)  
-        dildo:0, //1 small oraltraining,
-        pussy:0,
+        remainingNights: -1,
+        resourceForest: 5,   //number resource left
+        exploreForest:0,
         visitedTiles: [],mapReveal: [],
         dng:'', //current dungeon name
         prevLocation:'', nextLocation:'', //used for nav-logic
         dngMap:{} //dungeon map info
     };
-    let Lab={}
-    let Known={
+    let Lab={} //see brewInit
+    let Known={ //flags for things you know, see Data.ods
       recipes:{}  //'LustPotion:20%'
       ,places:{},study:{}
     }
@@ -44,7 +41,7 @@ return Lib; }(window.gm.AlchemyDef || {}));
 
 window.gm.printGoto=function(location,time,energy,alias){
     let msg='';
-    msg=window.gm.printLink((alias===''?location:alias),
+    msg=window.gm.printLink((alias===''?location:alias)+((time>0)?' ('+time+'min)':''),
     "window.gm.addTime("+time.toString()+");window.story.show(\""+location+"\");")
     return(msg);
 };
@@ -54,8 +51,8 @@ window.gm.__Ingrds={};//temp- lookuptable
 window.gm.brewInit=function(mode) {
     let z,y,x;
     window.story.state.Lab={
-        mode:mode,
-        in:[],      //'herbGreen','herbBlue',''
+        mode:mode, //
+        ingr:[],      //'herbGreen','herbBlue',''
         state:0,
         startTime:''
       };
@@ -72,12 +69,12 @@ window.gm.brewInit=function(mode) {
 };
 window.gm.brewUpdateView=function(){
     let entry,panel,all =window.gm.AlchemyDef;
-    let z,y,x=window.gm.player.Inv.getAllIds(),s=window.story.state,lab=window.story.state.Lab;
+    let z,y,x=window.gm.player.Inv.getAllIds(),s=window.story.state,Lab=window.story.state.Lab;
     panel=document.getElementById('recipe');
     for(var i=panel.childNodes.length-1;i>=0;i-- ) {
         panel.removeChild(panel.childNodes[i]);
     } 
-    if(lab.mode==='known' || lab.mode==='potential' || lab.mode==='unavailable') {
+    if(Lab.mode==='known' || Lab.mode==='potential' || Lab.mode==='unavailable') {
         //if recipe is selected, replace all ingredients in batch
         let list=s.Known.recipes,ids=Object.keys(list);
         for(var el of ids){ //list recipes
@@ -87,18 +84,18 @@ window.gm.brewUpdateView=function(){
             z=1; //
             x.ingr.forEach(v=>(need[v]=((!need[v])?1:need[v]+1)));
             x.ingr.forEach(v=>(z=(!window.gm.__Ingrds[v] || (window.gm.__Ingrds[v].cnt-need[v])<0)?0:z));
-            if((lab.mode==='potential' && list[el]<1) || 
-                (lab.mode==='known' && list[el]>=1)) {
+            if((Lab.mode==='potential' && list[el]<1) || 
+                (Lab.mode==='known' && list[el]>=1)) {
                // 
                entry = document.createElement('button');
                entry.addEventListener('click',(function(list){
-                   window.story.state.Lab.in=[],list.forEach(v=>(window.story.state.Lab.in.push(v)));
+                   window.story.state.Lab.ingr=[],list.forEach(v=>(window.story.state.Lab.ingr.push(v)));
                    window.gm.printOutput("");window.gm.brewUpdateView();}).bind(null,x.ingr));
                entry.id=_id;
                entry.textContent=x.name;
                entry.disabled=(z<1); //todo show disabled reason
                panel.appendChild(entry);
-            } else if(lab.mode==='unavailable' && (z<1)){
+            } else if(Lab.mode==='unavailable' && (z<1)){
                 entry = document.createElement('button');
                 entry.addEventListener('click',(function(list){
                    let x='requires ';list.forEach(v=>(x+=v+", "));
@@ -114,7 +111,7 @@ window.gm.brewUpdateView=function(){
             const _id=x[i];
             y=window.gm.__Ingrds[_id];
             z=y.cnt;
-            window.story.state.Lab.in.forEach(v=>z-=((v===_id)?1:0));
+            window.story.state.Lab.ingr.forEach(v=>z-=((v===_id)?1:0));
             entry = document.createElement('button');
             entry.addEventListener('click',window.gm.brewAddIngr);
             entry.id=_id;
@@ -129,30 +126,30 @@ window.gm.brewUpdateView=function(){
     } 
     entry = document.createElement('p');
     entry.textContent=""
-    for(var i=0;i<lab.in.length;i++){
-        entry.textContent+="-["+window.gm.__Ingrds[lab.in[i]].name+"]-";
+    for(var i=0;i<Lab.ingr.length;i++){
+        entry.textContent+="-["+window.gm.__Ingrds[Lab.ingr[i]].name+"]-";
     }
     panel.appendChild(entry);
 };
 //add an Ingredient to free slot
 window.gm.brewAddIngr=function(evt){
-    let lab=window.story.state.Lab;
-    if(lab.in.length>=3) return;
+    let Lab=window.story.state.Lab;
+    if(Lab.ingr.length>=3) return;
     /*let x=parseInt(evt.target.textContent.substr(evt.target.textContent.lastIndexOf(': ')+2)); //todo ugly
     x-=1;
     evt.target.textContent=evt.target.textContent.substr(0,evt.target.textContent.lastIndexOf(': ')+2)+x;
     evt.target.disabled=(x<1);*/
-    lab.in.push(evt.target.id);
+    Lab.ingr.push(evt.target.id);
     window.gm.printOutput("");
     window.gm.brewUpdateView();
 }
 //
 window.gm.brewCraftPotion=function(){
-    let list,all=window.gm.AlchemyDef,lab=window.story.state.Lab,known=window.story.state.Known.recipes;
+    let list,all=window.gm.AlchemyDef,Lab=window.story.state.Lab,known=window.story.state.Known.recipes;
     let res={OK:false,msg:'epic fail'};
     list=Object.keys(all);
     for(var el of list){
-        if(window.gm.util.arrayEquals(lab.in,all[el].ingr)) { //find recipe match
+        if(window.gm.util.arrayEquals(Lab.ingr,all[el].ingr)) { //find recipe match
             if(!known[el]) { //new
                 res.OK=true;
                 res.msg="This looks promissing. But some improvements are necessary.</br>"
@@ -173,8 +170,37 @@ window.gm.brewCraftPotion=function(){
             }
         }
     }
-    lab.in.forEach(v=>(window.gm.player.Inv.removeItem(v,1)));
-    lab.in=[];
-    window.gm.brewInit(lab.mode);
+    Lab.ingr.forEach(v=>(window.gm.player.Inv.removeItem(v,1)));
+    Lab.ingr=[];
+    window.gm.brewInit(Lab.mode);
     window.gm.printOutput(res.msg);
+}
+window.gm.qID = window.gm.qID || {};
+window.gm.questDef = window.gm.questDef || {};
+{
+    let quest = new Quest("qGarden","Green thumb","");
+    quest.addMileStone(new QuestMilestone(1,"","Find the garden.",QuestMilestone.NOP,QuestMilestone.HIDDEN));
+    quest.addMileStone(new QuestMilestone(100,"","The garden is a mess and you dont have a use for it. So why bother?.",QuestMilestone.NOP));
+    quest.addMileStone(new QuestMilestone(200,"","Get some tools to clear the garden from the rubble..",QuestMilestone.NOP));
+    window.gm.qID.qGarden=quest.id;window.gm.questDef[quest.id]= quest;
+
+    quest = new Quest("qBarJob","Serve'em drinks","");
+    quest.addMileStone(new QuestMilestone(1,"","Find the Inn.",QuestMilestone.NOP,QuestMilestone.HIDDEN));
+    quest.addMileStone(new QuestMilestone(100,"","Maybe the owner of the Inn has a job for you?.",QuestMilestone.NOP));
+    quest.addMileStone(new QuestMilestone(200,"","Earn money by working as a waitress.",QuestMilestone.NOP));
+    quest.addMileStone(new QuestMilestone(300,"","Work at least 6 shifts between Monday and sunday.",QuestMilestone.NOP));
+    quest.addMileStone(new QuestMilestone(10000,"","???",QuestMilestone.NOP));
+    window.gm.qID[quest.id]=quest.id;window.gm.questDef[quest.id]= quest;
+
+    quest = new Quest("qTraderThomas","A Trader to trade","");
+    quest.addMileStone(new QuestMilestone(1,"","Find the Traders house.",QuestMilestone.NOP,QuestMilestone.HIDDEN));
+    quest.addMileStone(new QuestMilestone(100,"","Ask around for the traders absence.",QuestMilestone.NOP));
+    quest.addMileStone(new QuestMilestone(200,"","Find the lookout point.",QuestMilestone.NOP));
+    quest.addMileStone(new QuestMilestone(300,"","Report back to the innkeeper.",QuestMilestone.NOP));
+    quest.addMileStone(new QuestMilestone(500,"","Find the traders carriage on the road to Redwick.",QuestMilestone.NOP));
+    quest.addMileStone(new QuestMilestone(900,"","Return with Thomas to his store.",QuestMilestone.NOP));
+    quest.addMileStone(new QuestMilestone(1000,"","NYI",QuestMilestone.NOP));
+    quest.addMileStone(new QuestMilestone(10000,"","???",QuestMilestone.NOP));
+    window.gm.qID.qTraderThomas=quest.id;window.gm.questDef[quest.id]= quest;
+
 }
